41548d8c454e2eb82bab62804915fd08f3b06e07
fiwon123 <felipeinoue@yahoo.com.br>
2020-03-23 00:12:36 -0300

    Finish Movement Actors

--- .cache/behavior-hx/Design_30_30_FollowTarget.hx
+++ .cache/behavior-hx/Design_30_30_FollowTarget.hx
@@ -61,27 +61,95 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class Design_8_8_MovementPointerMouse extends ActorScript
+class Design_30_30_FollowTarget extends ActorScript
 {
+	public var _TargetActor:Actor;
+	public var _DistanceX:Float;
+	public var _DistanceY:Float;
+	public var _Distance:Float;
+	public var _Direction:Float;
+	public var _Speed:Float;
+	public var _Margin:Float;
+	public var _Easing:Bool;
+	public var _MinimumEasingSpeed:Float;
+	public var _ScreenDiagonal:Float;
+	public var _StopwhenColliding:Bool;
+	public var _Collided:Bool;
 	
 	
 	public function new(dummy:Int, actor:Actor, dummy2:Engine)
 	{
 		super(actor);
 		nameMap.set("Actor", "actor");
+		nameMap.set("Target Actor", "_TargetActor");
+		nameMap.set("Distance X", "_DistanceX");
+		_DistanceX = 0.0;
+		nameMap.set("Distance Y", "_DistanceY");
+		_DistanceY = 0.0;
+		nameMap.set("Distance", "_Distance");
+		_Distance = 0.0;
+		nameMap.set("Direction", "_Direction");
+		_Direction = 0.0;
+		nameMap.set("Speed", "_Speed");
+		_Speed = 30.0;
+		nameMap.set("Margin", "_Margin");
+		_Margin = 0.0;
+		nameMap.set("Easing", "_Easing");
+		_Easing = true;
+		nameMap.set("Minimum Easing Speed", "_MinimumEasingSpeed");
+		_MinimumEasingSpeed = 5.0;
+		nameMap.set("Screen Diagonal", "_ScreenDiagonal");
+		_ScreenDiagonal = 0.0;
+		nameMap.set("Stop when Colliding", "_StopwhenColliding");
+		_StopwhenColliding = true;
+		nameMap.set("Collided", "_Collided");
+		_Collided = false;
 		
 	}
 	
 	override public function init()
 	{
 		
+		/* ======================== When Creating ========================= */
+		_ScreenDiagonal = Math.sqrt((Math.pow(getScreenWidth(), 2) + Math.pow(getScreenHeight(), 2)));
+		
 		/* ======================== When Updating ========================= */
 		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
 		{
 			if(wrapper.enabled)
 			{
-				actor.setXCenter(getMouseX());
-				actor.setYCenter(getMouseY());
+				if(((hasValue(_TargetActor)) && _TargetActor.isAlive()))
+				{
+					_DistanceX = (_TargetActor.getXCenter() - actor.getXCenter());
+					_DistanceY = (_TargetActor.getYCenter() - actor.getYCenter());
+					_Distance = Math.sqrt((Math.pow(_DistanceX, 2) + Math.pow(_DistanceY, 2)));
+					_Direction = Utils.DEG * (Math.atan2(_DistanceY, _DistanceX));
+					if(((_Distance > _Margin) && !((_StopwhenColliding && _Collided))))
+					{
+						if(_Easing)
+						{
+							actor.setVelocity(_Direction, (_MinimumEasingSpeed + (_Speed * ((_Distance - _Margin) / _ScreenDiagonal))));
+						}
+						else
+						{
+							actor.setVelocity(_Direction, _Speed);
+						}
+					}
+					else
+					{
+						actor.setVelocity(0, 0);
+					}
+					_Collided = false;
+				}
+			}
+		});
+		
+		/* ======================== Specific Actor ======================== */
+		addCollisionListener(actor, function(event:Collision, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled && (_TargetActor == event.otherActor))
+			{
+				_Collided = true;
 			}
 		});
 		
